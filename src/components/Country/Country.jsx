2280b0b219ea92265d89c7297a835bace2e4765b
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-03-25 11:05:17 +0600

    added population

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const Country = ({country}) => {
-    const {name,flags} = country;
+    const {name,flags, population} = country;
     return (
         <div>
             <h3>Official Name : {name.official} </h3>
             <h3> common Name: {name.common} </h3>
+            <h3>Population : {population.population}</h3>
             <img src={flags.flags.png} alt="" />
         </div>
     );
