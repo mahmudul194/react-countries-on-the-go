e6bbccb2fed43e4905609af7156e703189067265
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-03-25 11:17:12 +0600

    added region and also styled the the county div

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
 const Country = ({country}) => {
-    const {name,flags, population} = country;
+    const {name,flags, population, region} = country;
     return (
-        <div>
-            <h3>Official Name : {name.official} </h3>
-            <h3> common Name: {name.common} </h3>
-            <h3>Population : {population.population}</h3>
-            <img src={flags.flags.png} alt="" />
+        <div style={{
+            border : '2px solid white',
+            borderRadius : '10px',
+            margin : '20px',
+            padding : '20px'
+        }}>
+            <img src={flags?.flags?.png} alt="" />
+            <h3>Official Name : {name?.official} </h3>
+            <h3> common Name: {name?.common} </h3>
+            <h3>Population : {population?.population}</h3>
+            <h3>Region: {region?.region}</h3>
         </div>
     );
 };
