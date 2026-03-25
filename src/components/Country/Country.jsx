92456f84914c4c1b643f067ff7ef70608d254b56
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-03-25 11:56:07 +0600

    added more styles to the country file

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -4,16 +4,24 @@ const Country = ({country}) => {
     const {name,flags, population, region} = country;
     return (
         <div style={{
-            border : '2px solid white',
-            borderRadius : '10px',
-            margin : '20px',
-            padding : '20px'
-        }}>
-            <img src={flags?.flags?.png} alt="" />
-            <h3>Official Name : {name?.official} </h3>
-            <h3> common Name: {name?.common} </h3>
-            <h3>Population : {population?.population}</h3>
-            <h3>Region: {region?.region}</h3>
+                border : '2px solid white',
+                borderRadius : '10px',
+                margin : '20px',
+                padding : '20px',
+                display : 'flex',
+                alignItems : 'center',
+                justifyContent : 'space-around',
+                fontSize : '18px'
+            }}>
+            <div>
+                <img src={flags?.flags?.png} alt="" />
+            </div>
+            <div>
+                <h3>Official Name : {name?.official} </h3>
+                <h3> common Name: {name?.common} </h3>
+                <h3>Population : {population?.population}</h3>
+                <h3>Region: {region?.region}</h3>
+            </div>
         </div>
     );
 };
