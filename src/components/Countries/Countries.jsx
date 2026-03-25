e2ba83f340ef901b583ccb440c8e4de10529d9b6
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-03-25 10:28:59 +0600

    created countries file

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>zIn the countries</h1>
+        </div>
+    );
+};
+
+export default Countries;
