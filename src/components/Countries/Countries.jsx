fde65af9a881c9f86ef64ee55cbafe72576e84d7
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-03-25 10:55:47 +0600

    looped through every country to load them

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,12 +1,15 @@
 import React, { use } from 'react';
+import Country from '../Country/Country';
 
 const Countries = ({countriesPromise}) => {
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries;
-    console.log(countries)
     return (
         <div>
             <h1>In the countries : {countries.length} </h1>
+            {
+                countries.map(country => <Country country={country} ></Country>)
+            }
         </div>
     );
 };
