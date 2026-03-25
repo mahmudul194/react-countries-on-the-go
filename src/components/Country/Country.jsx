4c69b8797fa63e323d4f83b2653f5ea08f014924
Md. Mahmudul Hoque Rifat <mahmudulhaquerifat@gmail.com>
2026-03-25 11:02:55 +0600

    fetched the images and the common names from the country json

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const Country = ({country}) => {
-    const {name} = country;
+    const {name,flags} = country;
     return (
         <div>
-            <h3>Name : {name.official} </h3>
+            <h3>Official Name : {name.official} </h3>
+            <h3> common Name: {name.common} </h3>
+            <img src={flags.flags.png} alt="" />
         </div>
     );
 };
